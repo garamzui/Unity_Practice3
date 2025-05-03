6b87040b0c762794061dd5a5e4452fbbec46ae6b
garamzui <forsave121@gmail.com>
2025-05-04 00:49:38 +0900

    add

--- Assets/Scripts/Entity/PlayerController.cs
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -1,22 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : BaseController
 {
+    private GameManager gameManager;
     private Camera camera;
 
-    protected override void Start()
+    public void Init(GameManager gameManager)
     {
-        base.Start();
+        this.gameManager = gameManager;
         camera = Camera.main;
     }
+
 
     protected override void HandleAction()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        movementDirection = new Vector2 (horizontal, vertical).normalized;
+        movementDirection = new Vector2(horizontal, vertical).normalized;
 
         Vector2 mousePosition = Input.mousePosition;
         Vector2 worldPos = camera.ScreenToWorldPoint(mousePosition);
@@ -26,8 +26,17 @@ public class PlayerController : BaseController
         {
             lookDirection = Vector2.zero;
         }
-        else { lookDirection = lookDirection.normalized; }
+        else
+        {
+            lookDirection = lookDirection.normalized;
+        }
 
         isAttacking = Input.GetMouseButton(0);
+    }
+
+    public override void Death()
+    {
+        base.Death();
+        gameManager.GameOver();
     }
 }
--- Assets/Scripts/Weapon/ProjectileController.cs
+++ Assets/Scripts/Weapon/ProjectileController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ProjectileController : MonoBehaviour
@@ -17,6 +15,8 @@ public class ProjectileController : MonoBehaviour
     private SpriteRenderer spriteRenderer;
 
     public bool fxOnDestory = true;
+
+    private ProjectileManager projectileManager;
 
     private void Awake()
     {
@@ -42,26 +42,42 @@ public class ProjectileController : MonoBehaviour
         _rigidbody.velocity = direction * rangeWeaponHandler.Speed;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)//충돌 가능한지 판단하는 함수
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (levelCollisionLayer.value == (levelCollisionLayer.value | (1 << collision.gameObject.layer)))
         {
-            DestroyProjectile(collision.ClosestPoint/*가까운부분*/(transform.position) - direction * .2f, fxOnDestory);
+            DestroyProjectile(collision.ClosestPoint(transform.position) - direction * .2f, fxOnDestory);
         }
         else if (rangeWeaponHandler.target.value == (rangeWeaponHandler.target.value | (1 << collision.gameObject.layer)))
         {
+            ResourceController resourceController = collision.GetComponent<ResourceController>();
+            if (resourceController != null)
+            {
+                resourceController.ChangeHealth(-rangeWeaponHandler.Power);
+                if (rangeWeaponHandler.IsOnKnockback)
+                {
+                    BaseController controller = collision.GetComponent<BaseController>();
+                    if (controller != null)
+                    {
+                        controller.ApplyKnockback(transform, rangeWeaponHandler.KnockbackPower, rangeWeaponHandler.KnockbackTime);
+                    }
+                }
+            }
+
             DestroyProjectile(collision.ClosestPoint(transform.position), fxOnDestory);
         }
     }
 
 
-    public void Init(Vector2 direction, RangeWeaponHandler weaponHandler,ProjectileManager projectileManager)
+    public void Init(Vector2 direction, RangeWeaponHandler weaponHandler, ProjectileManager projectileManager)
     {
+        this.projectileManager = projectileManager;
+
         rangeWeaponHandler = weaponHandler;
 
         this.direction = direction;
         currentDuration = 0;
-        transform.localScale = Vector3.one * weaponHandler.BulletSize;
+        transform.localScale = Vector3.one * weaponHandler.Size;
         spriteRenderer.color = weaponHandler.ProjectileColor;
 
         transform.right = this.direction;
@@ -76,9 +92,11 @@ public class ProjectileController : MonoBehaviour
 
     private void DestroyProjectile(Vector3 position, bool createFx)
     {
+        if (createFx)
+        {
+            projectileManager.CreateImpactParticlesAtPostion(position, rangeWeaponHandler);
+        }
+
         Destroy(this.gameObject);
     }
-}
-
-
-
+}
